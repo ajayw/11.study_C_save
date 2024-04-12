3d145fa4209edd070165421355b1493957976b83
ajayw <ajay_w@qq.com>
2024-04-12 09:45:14 +0800

    添加代码

---
+++ 3_sum/sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+double root(double x,double y)
+{
+    if(x>y) return sqrt(x-y);
+    else return 0;
+}
+
+int sum(int a,int b)
+{
+    int sum;
+    sum = a + b;
+    return sum;
+}
+void main()
+{
+    int i,j;
+    printf("输入两个数");
+    scanf("%d%d",&i,&j);
+    printf("结果是：%d\n",sum(i,j));
+    printf("结果是：%f\n",root(i,j));
+}
