fccc8d1e1b0a0c5cbfb4f883aef10bbf429229fe
ajayw <ajay_w@qq.com>
2024-04-12 09:21:44 +0800

    Add a C project

---
+++ 1_Hello/hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// 函数返回两个数的最大公约数
+int getGCM(int a, int b)
+{
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+void main()
+{
+    int i,j;
+    printf("输入两个数，用空格隔开:");
+    scanf("%d%d",&i,&j);
+    printf("最大公约数为%d\n",getGCM(i,j));
+}
